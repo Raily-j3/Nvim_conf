8cfd8fe8ea243865d3cb29b9a30c500fc5ce9117
raily <railyj3@gmail.com>
2024-06-26 13:19:26 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-	"blazkowolf/gruber-darker.nvim",
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme gruber-darker]])
-	end,
+  'blazkowolf/gruber-darker.nvim',
+  priority = 1000,
+  config = function()
+    vim.cmd [[colorscheme gruber-darker]]
+  end,
 }
--- lua/plugins/treesistter.lua
+++ lua/plugins/treesistter.lua
@@ -1,14 +1,19 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local config = require 'nvim-treesitter.configs'
+      config.setup {
+        ensure_installed = {
+          'lua',
+          'vim',
+          'c',
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
+  },
 }
