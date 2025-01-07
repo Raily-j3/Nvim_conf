aae42793d228b1c476f43d93dc1f41590d8a021e
liruijie <liruijie@vyagoo.com>
2025-01-07 09:49:20 +0800

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
+-- return {
+--   'blazkowolf/gruber-darker.nvim',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd [[colorscheme gruber-darker]]
+--   end,
+-- }
 return {
-  'blazkowolf/gruber-darker.nvim',
-  priority = 1000,
-  config = function()
-    vim.cmd [[colorscheme gruber-darker]]
-  end,
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- vim.cmd([[colorscheme nordfox]])
+		vim.cmd([[colorscheme nightfox]])
+	end,
 }
