9ef90cfc878ae07fad0438407419b26c189ccb8e
liruijie <liruijie@vyagoo.com>
2025-01-07 15:17:37 +0800

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,11 +6,11 @@
 --   end,
 -- }
 return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- vim.cmd([[colorscheme nordfox]])
-		vim.cmd([[colorscheme nightfox]])
-	end,
+  'EdenEast/nightfox.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd [[colorscheme nordfox]]
+    -- vim.cmd([[colorscheme nightfox]])
+  end,
 }
