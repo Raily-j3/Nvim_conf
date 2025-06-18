148a123a2c288ad9185b53ca31b2935967eeb49b
Raily <railyj3@gmail.com>
2025-06-18 15:48:59 +0800

    listish

--- lua/plugins/listish.lua
+++ lua/plugins/listish.lua
@@ -1,19 +1,23 @@
 return {
-  "arsham/listish.nvim",
+  'arsham/listish.nvim',
   dependencies = {
-    "arsham/arshlib.nvim",
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    'arsham/arshlib.nvim',
+    'nvim-treesitter/nvim-treesitter-textobjects',
   },
   config = {
-		signs = false,
-		loclist = {
-		  open = "nil",
-		  on_cursor = "nil",
-		  add_note = "nil",
-		  clear = "nil",
-		  close = "nil",
-		  next = "nil",
-		  prev = "nil",
-		},
-	}
+    signs = false,
+    loclist = {
+      open = 'nil',
+      on_cursor = 'nil',
+      add_note = 'nil',
+      clear = 'nil',
+      close = 'nil',
+      next = 'nil',
+      prev = 'nil',
+    },
+    quickfix = {
+      on_cursor = '<C-q>', -- add current position to the list
+      add_note = '<leader>qn', -- add current position with your note to the list
+    },
+  },
 }
