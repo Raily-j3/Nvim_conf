978518cecfe76141e182bb0e23d7d3c53ca41540
Raily <railyj3@gmail.com>
2025-06-24 11:25:44 +0800

    local-highlight

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,22 +1,23 @@
 return {
-  "folke/which-key.nvim",
+  'folke/which-key.nvim',
   dependencies = {
-	'nvim-tree/nvim-web-devicons',
-	'echasnovski/mini.icons'
-	},
-  event = "VeryLazy",
+    'nvim-tree/nvim-web-devicons',
+    'echasnovski/mini.icons',
+  },
+  event = 'VeryLazy',
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    preset = 'helix',
   },
   keys = {
     {
-      "<leader>?",
+      '<leader>?',
       function()
-        require("which-key").show({ global = false })
+        require('which-key').show { global = false }
       end,
-      desc = "Buffer Local Keymaps (which-key)",
+      desc = 'Buffer Local Keymaps (which-key)',
     },
   },
 }
