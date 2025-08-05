a884a35bdf5f2d2e914d09ab0c2b9034a85ea8cd
Raily <railyj3@gmail.com>
2025-08-05 15:12:13 +0800

    scrollbar: disalble diagnostic

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,7 +1,11 @@
 return {
   'petertriho/nvim-scrollbar',
   config = function()
-    require('scrollbar').setup {}
+    require('scrollbar').setup {
+      handlers = {
+        diagnostic = false,
+      },
+    }
     require('scrollbar.handlers.gitsigns').setup()
   end,
 }
