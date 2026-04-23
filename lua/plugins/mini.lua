a40bf6b9b839ebe11c2774350825d7cb2dc2e0a1
liruijie <liruijie@vyagoo.com>
2026-04-23 16:35:36 +0800

    update

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,12 +4,32 @@ return {
     version = false,
     dependencies = {
       { 'nvim-mini/mini.pairs', version = false },
+      { 'nvim-mini/mini.comment', version = false },
       -- {'nvim-mini/mini.surround', version = false},
     },
 
     config = function()
       require('mini.ai').setup()
       require('mini.pairs').setup()
+      require('mini.comment').setup{
+		    -- Module mappings. Use `''` (empty string) to disable one.
+		  mappings = {
+			-- Toggle comment (like `gcip` - comment inner paragraph) for both
+			-- Normal and Visual modes
+			comment = '<leader>/',
+
+			-- Toggle comment on current line
+			comment_line = '<leader>/',
+
+			-- Toggle comment on visual selection
+			comment_visual = '<leader>/',
+
+			-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+			-- Works also in Visual mode if mapping differs from `comment_visual`
+			textobject = '<leader>/',
+		  },
+	  }
+
       -- require('mini.surround').setup()
     end,
   },
